5971b389c29902fc15ceda70093c0971903e5141
Abideen Olafimihan <olafimihana@gmail.com>
2023-04-19 15:54:25 +0530

    create desktop and mobile screen sizes for the home section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 import Delivery from "../../assets/delivery.png";
+import Herobg from "../../assets/heroBg.png";
 import { clx } from "../../utils/clx";
+import { imageData } from "../../utils/homeFourImages";
 
 const classes = clx(
   "mt-14 md:mt-[50px] lg:mt-[72px] p-4 md:p-8 md:px-16 w-full lg:h-[calc(100vh_-_73px)] grid grid-cols-1 md:grid-cols-2 gap-2"
@@ -10,6 +12,7 @@ const classes = clx(
 const Home = () => {
   return (
     <section className={classes} id="home">
+      {/* FIRST PART */}
       <div className="py-2 flex-1 flex flex-col gap-2 items-start">
         <div className="flex gap-2 items-center justify-center py-1 px-4 rounded-full bg-orange-100">
           <p className="text-orange-400 font-semibold text-[15px]">
@@ -25,9 +28,9 @@ const Home = () => {
         </div>
 
         <p className="text-[2rem] text-center md:text-left lg:text-[3.5rem] font-bold tracking-wider text-headingColor">
-          The Fastest Delivery to
+          The Fastest Delivery on{" "}
           <span className="text-[2.3rem] lg:text-[4rem] text-orange-600">
-            Your Door Step
+            a Click
           </span>
         </p>
 
@@ -39,13 +42,43 @@ const Home = () => {
 
         <button
           type="button"
-          className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:m-auto md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
+          className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:mx-auto md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
         >
           Order Now
         </button>
       </div>
 
-      <div className="py-2 bg-blue-400 flex-1">Hello</div>
+      {/* SECOND PART */}
+      <div className="py-2 bg-ble-400 flex-1 relative px-2">
+        <img
+          src={Herobg}
+          alt="hero-bg"
+          className="ml-auto w-full lg:w-[400px] h-[400px] lg:h-[470px]"
+        />
+
+        <div className="w-full h-full gap-2 flex flex-wrap items-center justify-center lg:px-10  absolute border border-red-500 top-0 left-0">
+          {imageData.map((image) => {
+            return (
+              <div className=" md:w-[150px] lg:w-[160px] lg:min-w-[160px] p-4 bg-cardOverlay backdrop-blur-md flex flex-col items-center justify-center rounded-2xl">
+                <img
+                  src={image.imageSrc}
+                  alt="icecream"
+                  className="w-20 lg:w-28 -mt-10 lg:-mt-20"
+                />
+                <p className="text-base text-textColor font-semibold">
+                  {image.name}
+                </p>
+                <p className="text-xs lg:text-sm font-semibold text-lightTextGray my-1 lg:my-2">
+                  {image.description}
+                </p>
+                <p className="text-sm font-semibold text-headingColor">
+                  <span className="text-xs text-red-600">$</span> {image.price}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </section>
   );
 };
---
+++ src/utils/homeFourImages.js
@@ -0,0 +1,35 @@
+import I1 from "../assets/i1.png";
+import F1 from "../assets/f1.png";
+import C3 from "../assets/c3.png";
+import Fi1 from "../assets/fi1.png";
+
+export const imageData = [
+  {
+    id: 1,
+    name: "Icecream",
+    description: "chocolate & vanilla",
+    price: 5.25,
+    imageSrc: I1,
+  },
+  {
+    id: 2,
+    name: "Strawberries",
+    description: "fresh strawberries",
+    price: 10.25,
+    imageSrc: F1,
+  },
+  {
+    id: 3,
+    name: "Chicken kebab",
+    description: "mixed kebab plate",
+    price: 8.25,
+    imageSrc: C3,
+  },
+  {
+    id: 4,
+    name: "Fish kebab",
+    description: "mixed fish kebab",
+    price: 7.25,
+    imageSrc: Fi1,
+  },
+];
